2d6a389641c9542cfa620f50c33c5fac74e53fd0
Maria Lipina <m.o.lip714@gmail.com>
2021-12-14 23:41:59 +0300

    создание колоды и тасовка

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,8 +1,5 @@
 ﻿// 1. Мария - Метод создания колоды для игры (с тасовкой).
 
-/*                 Туз   Два  Три  Четыре  Пять  Шесть  Семь  Восемь  Девять   Десять   Валет   Дама   Король*/
-int [] suitCards = {1,    2,   3,    4,      5,    6,     7,    8,      9,       10,     11,     12,     13};
-
 int [] Deck ()
 {
     int [] result = new int [52];
@@ -26,42 +23,9 @@ for (int i = 0; i < deck.Length; i++)
 }
 }
 
-string PrintMyArray(int[] collect)
-{
-    string outputString = String.Empty;
-    for (int pos = 0; pos < collect.Length; pos++) outputString += $"{collect[pos]} ";
-    return outputString;
-}
-
-int [] FindCount (int [] array, int [] values)
-{
-int [] count = new int [values.Length];
-for (int h = 0; h < values.Length; h++)
-{
-for (int i = 0; i < array.Length; i++)
-{
-    if (values[h] == array[i]) count[h]++;
-}
-}
-return count;
-}
-
-string PrintCounts (int [] count, int [] values)
-{
-    string outputString = String.Empty;
-    for (int i = 0; i < count.Length; i++)
-    {
-        outputString += $"{values[i]} встречается {count[i]} раз;\n";
-    }
-return outputString;
-}
-
 int [] deck = Deck ();
 Shuffle(deck);
-Console.WriteLine(PrintMyArray(deck));
 
-int [] cardsCheck = FindCount(deck, suitCards);
-Console.WriteLine(PrintCounts(cardsCheck, suitCards));
 
 // 2. Юрий - Метод названия карт.
 // 3. Семён - Метод вытягивания карт из колоды (подсчет очков, подсчет использованный карт).
